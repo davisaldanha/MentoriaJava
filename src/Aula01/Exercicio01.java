04ae911b5afe2aa06b1d3d036e933b8db2e05e20
Davi Saldanha <davisaldanha.ti@gmail.com>
2023-11-26 22:44:19 -0300

    First Commit

---
+++ src/Aula01/Exercicio01.java
@@ -0,0 +1,31 @@
+
+package Aula01;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        
+        /*
+        Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. 
+        Caso sexo seja “F” e estado civil seja “CASADA”, solicitar o tempo de casada (anos).
+     
+        */
+        Scanner scan = new Scanner(System.in);
+        String nome, sexo, estadoCivil;
+        
+        System.out.print("Digite o seu nome: ");
+        nome = scan.next();
+        System.out.print("Digite o seu sexo [F] ou [M]: ");
+        sexo = scan.next();
+        System.out.println("Digite o seu estado civil [CASADO] ou [SOLTEIRO]");
+        estadoCivil = scan.next();
+               
+        
+        if(sexo.equals("F") && estadoCivil.equals("CASADO")){
+            System.out.println(scan.next());            
+        }
+    }
+    
+}
---
+++ src/Aula01/MentoriaJava.java
@@ -0,0 +1,96 @@
+package Aula01;
+
+public class MentoriaJava {
+
+    public static void main(String[] args) {
+        
+       /*Tipos Primitivos
+            - byte
+            - short
+            - char
+            - int
+            - long
+            - float
+            - double        
+        */
+       
+       /*
+        Java Casting - a possibilidade de converter tipos de dados
+       
+        **Casting Automático**
+            byte -> short -> char -> int -> long -> float -> double
+       */
+       int x = 10;
+       double y = x;
+       
+       System.out.println(y);
+       
+       /*
+        **Casting Manual**
+            double -> float -> long -> int -> char -> short -> byte
+       */
+       double a = 10.55;
+       long b = (long) a;
+       
+        System.out.println(b);
+        
+     /*
+        **Operadores Aritméticos**
+            soma -> +
+            subtração -> -
+            multiplicação -> *
+            divisão -> /
+            resto da divisão -> %
+        
+        **Operadores Relacionais**
+            igual -> ==
+            menor -> <
+            maior -> <
+            maior igual -> >=
+            menor igual -> <=
+            diferente -> !=
+        
+        **Operadores Lógicos**
+            AND -> &&
+            OR -> ||
+            NOT -> ! 
+        */ 
+     
+     /*
+     
+        Estrutura Condicional Simples -> IF
+        Estrutura Condicional Composta -> IF..ELSE
+        Estrutura Condicional Encadeada -> IF..ELSE..IF
+     
+     */
+     
+     double media = 5.0, frequencia = 80.0;
+     
+     
+     //ESTRUTURA CONDICIONAL SIMPLES
+     if(media>=7)
+        System.out.println("APROVADO!");
+    
+    //ESTRUTURA CONDICIONAL COMPOSTA
+     if(media>=7)
+        System.out.println("APROVADO!");
+     else
+        System.out.println("REPROVADO!");
+    
+    //ESTRUTURA CONDICIONAL ENCADEADA
+    if(media>=7 && frequencia>=75){
+        System.out.println("APROVADO!");
+        if(media>=9){
+            System.out.println("ALUNO DESTAQUE!");
+        }
+    }else{
+        if(media>=7 && frequencia>=60){
+           System.out.println("CONSELHO DE CLASSE!"); 
+        }else{
+            System.out.println("REPROVADO!");
+        }
+    }  
+   
+    }
+    
+}
